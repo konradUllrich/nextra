88f53e78c533a3f9ded55a78e940eeb50d1ee6c2
KonradUllrich <konrad.ullrich@moysies.de>
2023-08-03 21:02:14 +0200

    nofollow, noindex

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-
   logo: <span>INTREXX</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
+
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  editLink:{text:'edit'},
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  editLink: { text: "edit" },
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
 
+  useNextSeoProps: () => ({ nofollow: true, noindex: true }),
+};
 
-}
-
-export default config
+export default config;
