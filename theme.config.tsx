3befaa12cf311ff624cd9ae5fe3d69c16e8d2266
KonradUllrich <konrad.ullrich@moysies.de>
2023-08-03 20:39:51 +0200

    init

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+
+  logo: <span>INTREXX</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
   chat: {
     link: 'https://discord.com',
   },
+  editLink:{text:'edit'},
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+
+
 }
 
 export default config
